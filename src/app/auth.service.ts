c2ea67f7b47fc489506e19e892c992e7f273a56c
tanvir <hossaint33@gmail.com>
2017-10-26 19:00:33 +0600

    showing and Hiding the specific admin links or routes if current user dont have a admin role or current user admin role is false

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,17 @@
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
 import { ReturnStatement } from '@angular/compiler';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private userService: UserService) { 
     this.user$ = afAuth.authState;
   }
 
@@ -21,5 +24,10 @@ export class AuthService {
 
   logout(){
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser>{
+    return this.user$
+        .switchMap(user => this.userService.get(user.uid));
   }
 }
--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,3 +1,4 @@
+import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent  {
+  appUser: AppUser;
 
-  constructor(public auth: AuthService) {
-    
+  constructor(private auth: AuthService) {
+    auth.appUser$.subscribe(appUser => this.appUser = appUser)
    }
 
   logout(){
